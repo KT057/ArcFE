ed2de8c3a9c0cb7dc19d986b2251b92be0c09ff1
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-05-31 08:22:08 +0900

    finish product002 for toggle

--- apps/ui/src/Toggle/Product002/script/index.ts
+++ apps/ui/src/Toggle/Product002/script/index.ts
@@ -1,6 +1,29 @@
-import gsap from 'gsap';
+document.addEventListener('DOMContentLoaded', () => {
+  const boxes = document.querySelectorAll<HTMLElement>('.js-pd002');
 
-(() => {
-  console.log('test');
-  console.log(gsap);
-})();
+  for (const box of Array.from(boxes)) {
+    box.addEventListener('click', () => {
+      console.log(box.classList);
+
+      if (
+        !box.classList.contains('is-open') &&
+        !box.classList.contains('is-close')
+      ) {
+        box.classList.add('is-open');
+        return;
+      }
+
+      if (box.classList.contains('is-close')) {
+        box.classList.remove('is-close');
+        box.classList.add('is-open');
+        return;
+      }
+
+      if (box.classList.contains('is-open')) {
+        box.classList.remove('is-open');
+        box.classList.add('is-close');
+        return;
+      }
+    });
+  }
+});
