df13f8d9f4bf6e4349b41f1b402b2a67fb3393a9
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-05 07:55:28 +0900

    fix

---
+++ apps/ui/src/components/Icon/index.stories.js
@@ -0,0 +1,31 @@
+import '../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'Icon',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide">
+      ${component}
+    </div>
+  `;
+};
+
+export const Default = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2171-3&t=1vceUJz8UfWSxwKe-4'
+      }
+    ]
+  }
+};
