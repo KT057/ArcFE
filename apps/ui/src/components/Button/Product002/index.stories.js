624d77983bcc482e2e10268c21f11b1da252cac2
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-07 08:07:58 +0900

    add: 002

---
+++ apps/ui/src/components/Button/Product002/index.stories.js
@@ -0,0 +1,31 @@
+import '../../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'Button',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide">
+      ${component}
+    </div>
+  `;
+};
+
+export const Product002 = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2202-225&t=P6e7zlNuzHtdSf0m-4'
+      }
+    ]
+  }
+};
