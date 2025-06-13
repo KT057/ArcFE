56a5e09044c1a1f9a7fa9764b746d57199ef0236
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-13 15:59:45 +0900

    create: Frameicon

---
+++ apps/ui/src/components/FrameIcon/Product001/index.stories.js
@@ -0,0 +1,33 @@
+import '../../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'FrameIcon',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide">
+      <div class="site-wide__w300">
+        ${component}
+      </div>
+    </div>
+  `;
+};
+
+export const Product001 = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2268-413&t=SWxYNzbne83z0sVs-4'
+      }
+    ]
+  }
+};
