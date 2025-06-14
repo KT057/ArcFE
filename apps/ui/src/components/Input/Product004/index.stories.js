6c7a3723fb4b6a6b9b56234cbdbf319ad019b399
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-14 13:44:41 +0900

    create Product004

---
+++ apps/ui/src/components/Input/Product004/index.stories.js
@@ -0,0 +1,33 @@
+import '../../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'Input',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide">
+      <div class="site-wide__w300">
+        ${component}
+      </div>
+    </div>
+  `;
+};
+
+export const Product004 = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2261-159&t=QOIcXdTsYQM2nWr7-4'
+      }
+    ]
+  }
+};
