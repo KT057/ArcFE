6dd3c8f64874b60cb9360becd82bff7e3c7f91af
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-15 10:56:40 +0900

    create TextFiled

---
+++ apps/ui/src/components/TextField/Product004/index.stories.js
@@ -0,0 +1,33 @@
+import '../../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'TextField',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide">
+      <div class="site-wide__w300">
+        ${component}
+      </div>
+    </div>
+  `;
+};
+
+export const Product004 = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2294-373&t=CG3Uz8S42iBVKtUK-4'
+      }
+    ]
+  }
+};
