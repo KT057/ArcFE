f939c44885aabd173eb68fce8e9614be3d97fa67
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-19 17:56:13 +0900

    create: Balloon compoent

---
+++ apps/ui/src/components/Balloon/Product001/index.stories.js
@@ -0,0 +1,31 @@
+import '../../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'Balloon',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide" style="padding: 100px; display: flex; justify-content: center; align-items: center; gap: 50px;">
+      ${component}
+    </div>
+  `;
+};
+
+export const Product001 = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2379-463&t=XelstliegjgV8J7Y-4'
+      }
+    ]
+  }
+};
