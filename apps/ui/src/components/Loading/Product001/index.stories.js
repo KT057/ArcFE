eabd4324e11d7ee89bc5d28268aa13a37c954ba9
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-20 07:28:12 +0900

    create Loading

---
+++ apps/ui/src/components/Loading/Product001/index.stories.js
@@ -0,0 +1,31 @@
+import '../../../shared/styles/common.css';
+import runScript from '!!raw-loader!../../../shared/scripts/common.js';
+import { withRunScript } from 'storybook-addon-run-script/html';
+import Html from './index.html';
+
+export default {
+  title: 'Loading',
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide">
+      ${component}
+    </div>
+  `;
+};
+
+export const Default = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: 'link',
+        url: 'https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2380-485&t=RGFLTMKlDzmNnPk1-4'
+      }
+    ]
+  }
+};
