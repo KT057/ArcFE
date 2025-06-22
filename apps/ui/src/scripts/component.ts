4a025c9cffdfec7097d942434558e5a894145eaf
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-22 16:33:33 +0900

    create Product005

--- apps/ui/src/scripts/component.ts
+++ apps/ui/src/scripts/component.ts
@@ -2,6 +2,7 @@ import { accordionProduct001 } from "../components/Accordion/Product001/script";
 import { accordionProduct002 } from "../components/Accordion/Product002/script";
 import { accordionProduct003 } from "../components/Accordion/Product003/script";
 import { accordionProduct004 } from "../components/Accordion/Product004/script";
+import { accordionProduct005 } from "../components/Accordion/Product005/script";
 import { buttonProduct002 } from "../components/Button/Product002/script";
 import { toggleProduct002 } from "../components/Toggle/Product002/script";
 
@@ -12,4 +13,5 @@ export const componentScripts = () => {
   accordionProduct002();
   accordionProduct003();
   accordionProduct004();
+  accordionProduct005();
 };
