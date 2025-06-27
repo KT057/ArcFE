b3be3a70ce3efe5f00d10306f9b3029ee86f749d
kichikawa057B <kota.ichikawa@blueish.co.jp>
2025-06-28 07:46:53 +0900

    move

---
+++ apps/ui/src/components/Basic/Balloon/Product001/index.stories.js
@@ -0,0 +1,31 @@
+import "../../../../common/styles/common.css";
+import runScript from "!!raw-loader!../../../../common/scripts/common.js";
+import { withRunScript } from "storybook-addon-run-script/html";
+import Html from "./index.html";
+
+export default {
+  title: "Basic/Balloon",
+  decorators: [withRunScript(runScript)]
+};
+
+const Template = (component) => {
+  return `
+    <div class="site-wide" style="padding: 100px; display: flex; justify-content: center; align-items: center; gap: 50px;">
+      ${component}
+    </div>
+  `;
+};
+
+export const Product001 = {
+  render: () => {
+    return Template(Html);
+  },
+  parameters: {
+    design: [
+      {
+        type: "link",
+        url: "https://www.figma.com/design/YUbTzje2jUbb0Yh5ycNx2P/ui-catalog?node-id=2380-403&t=XelstliegjgV8J7Y-4"
+      }
+    ]
+  }
+};
--- apps/ui/src/scripts/component.ts
+++ apps/ui/src/scripts/component.ts
@@ -1,8 +1,8 @@
-import { accordionProduct001 } from "../components/Accordion/Product001/script";
-import { accordionProduct002 } from "../components/Accordion/Product002/script";
-import { accordionProduct003 } from "../components/Accordion/Product003/script";
-import { accordionProduct004 } from "../components/Accordion/Product004/script";
-import { accordionProduct005 } from "../components/Accordion/Product005/script";
+import { accordionProduct001 } from "../components/Basic/Accordion/Product001/script";
+import { accordionProduct002 } from "../components/Basic/Accordion/Product002/script";
+import { accordionProduct003 } from "../components/Basic/Accordion/Product003/script";
+import { accordionProduct004 } from "../components/Basic/Accordion/Product004/script";
+import { accordionProduct005 } from "../components/Basic/Accordion/Product005/script";
 import { buttonProduct002 } from "../components/Button/Product002/script";
 import { toggleProduct002 } from "../components/Toggle/Product002/script";
 
