8a18c25eea1fe520c2dc01c2ef05b193b9e4f2eb
kichikawa57 <k.ichikawa057@gmail.com>
2025-08-06 13:03:43 +0900

    create

--- apps/ui/src/components/Advanced/common/FadeInAndZoomImags/script/index.ts
+++ apps/ui/src/components/Advanced/common/FadeInAndZoomImags/script/index.ts
@@ -1,14 +1,23 @@
 import gsap from "gsap";
 
-const fadeInAndZoomImagesAnimation = ({nextIndex, images, image}: {nextIndex: () => number, images: HTMLDivElement[], image: HTMLDivElement}) => {
-  gsap.fromTo(image, {
+const fadeInAndZoomImagesAnimation = ({
+  nextIndex,
+  images,
+  image
+}: {
+  nextIndex: () => number;
+  images: HTMLDivElement[];
+  image: HTMLDivElement;
+}) => {
+  gsap.set(image, {
     scale: 1,
+    opacity: 1
+  });
+
+  const tween = gsap.to(image, {
+    scale: 1.2,
     opacity: 1,
-    zIndex: 0
-  }, {
-    scale: 1.4,
-    opacity: 1,
-    duration: 1.6,
+    duration: 4,
     ease: "power2.out",
     onComplete: () => {
       const index = nextIndex();
@@ -16,41 +25,51 @@ const fadeInAndZoomImagesAnimation = ({nextIndex, images, image}: {nextIndex: ()
       image.style.zIndex = "1";
       gsap.to(image, {
         opacity: 0,
-        duration: 1.2,
+        duration: 2,
         ease: "power2.out",
         onComplete: () => {
-          image.style.zIndex = "0"
+          image.style.zIndex = "0";
         }
       });
-      fadeInAndZoomImagesAnimation({nextIndex, images, image: nextImage});
+      fadeInAndZoomImagesAnimation({ nextIndex, images, image: nextImage });
+    },
+    onUpdate: () => {
+      const progress = Math.floor(tween.progress() * 100);
+      console.log(progress);
     }
   });
-}
+};
 
 export const fadeInAndZoomImages = () => {
   let currentIndex = 0;
-  const imageWrappers = document.querySelectorAll<HTMLDivElement>(".js-fade-in-and-zoom-images-wrapper");
+  const imageWrappers = document.querySelectorAll<HTMLDivElement>(
+    ".js-fade-in-and-zoom-images-wrapper"
+  );
 
   for (const imageWrapper of Array.from(imageWrappers)) {
-    const images = Array.from(imageWrapper.querySelectorAll<HTMLDivElement>(".cm-fade-in-and-zoom-images__content"));
+    const images = Array.from(
+      imageWrapper.querySelectorAll<HTMLDivElement>(
+        ".cm-fade-in-and-zoom-images__content"
+      )
+    );
 
     const maxIndexes = images.length - 1;
 
     const nextIndex = () => {
-      if (currentIndex >= maxIndexes) { 
+      if (currentIndex >= maxIndexes) {
         currentIndex = 0;
       } else {
         currentIndex++;
       }
-  
+
       return currentIndex;
-    }
+    };
 
     if (images) {
-      const firstImage = images[0]
+      const firstImage = images[0];
       if (!firstImage) return;
-      console
-      fadeInAndZoomImagesAnimation({nextIndex, images, image: firstImage});
+      firstImage.style.zIndex = "1";
+      fadeInAndZoomImagesAnimation({ nextIndex, images, image: firstImage });
     }
   }
 };
